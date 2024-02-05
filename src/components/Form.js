4f520a5e7441898fe9f2b68ec7a3c8b725461756
Nicole Yang <nicoleyry@outlook.com>
2024-02-05 15:33:31 +0800

    add submit button

--- src/components/Form.js
+++ src/components/Form.js
@@ -100,7 +100,14 @@ export default function Form({moment, setSubmitData, setIsSubmitted}) {
 				</fieldset>
 				
 				<fieldset className='submit'>
-					<button type="submit">Submit</button>
+					<hr className="submit-line" />
+					<div className="submit-wrapper" onClick={submitHandler}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 46 44">
+							<g fill="none" stroke="#FFF" strokeWidth="2">
+								<path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/>
+							</g>
+						</svg>
+					</div>
 				</fieldset>
 			</form>
 		</div>
