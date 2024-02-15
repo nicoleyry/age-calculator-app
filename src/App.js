5356f398993f6fa81c7ddf3d14868e3ccbf73fd2
Nicole Yang <nicoleyry@outlook.com>
2024-02-15 13:57:10 +0800

    add rel to footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className='App'>
 			<Wrapper />
 			<div className="attribution">
-				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
 				Coded by <a href="https://www.nicoleyry.com/">Nicole Yang</a>.
 			</div>
 		</div>
